470b0b7ad88884b5e67b78ffb13ae649cfd988e1
Ziyaddin Sadigov <ziyaddinsadigov@gmail.com>
2017-11-07 04:08:22 +0400

    Add dependencies and stats printing

--- cli.js
+++ cli.js
@@ -1,19 +1,27 @@
 #!/usr/bin/env node
 'use strict';
 const meow = require('meow');
-const hacktoberfestStats = require('HacktoberfestStats');
+const hacktoberfestStats = require('hacktoberfeststats');
 
 const cli = meow(`
 	Usage
-	  $ hacktoberfest <string>
+	  $ hacktoberfest <username> <year>
 
 	Examples
-	  $ hacktoberfest ziyaddin
-	  asdfg asdfg asdfg asdfg asdfg
+	  $ hacktoberfest ziyaddin 2017
+	  $ hacktoberfest sindresorhus 2015
+
 `);
 
 const input = cli.input;
 
-if(input.length > 0){
-	console.log(hacktoberfestStats(input[0]));
+if(input.length > 1){
+	hacktoberfestStats(input[0], input[1], function(hacktoberfestStats, callback, error) {
+		console.log("Name:", hacktoberfestStats.mainStats.Name);
+		console.log("Completed:", hacktoberfestStats.mainStats.Completed);
+		console.log("Progress:", hacktoberfestStats.mainStats.Progress);
+		console.log("Contributions:", hacktoberfestStats.mainStats.Contributions);
+	});
+}else{
+	console.log(cli.help);
 }
